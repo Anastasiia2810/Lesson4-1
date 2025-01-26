5cc9d9c39c8c051fcb5ede7135e52e41b4005806
Anastasiia2810 <ani_meduza_92@ukr.net>
2025-01-26 15:44:39 +0300

    added styles css

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const swiper = new Swiper('.slider-main-block', {
     loop: true,  
     // Navigation arrows
     navigation: {
-      nextEl: '.body-main-block__arrow.swiper-button-next',
-      prevEl: '.body-main-block__arrow.swiper-button-prev',
+      nextEl: '.slider-main-block__arrow.swiper-button-next',
+      prevEl: '.slider-main-block__arrow.swiper-button-prev',
     },
   });
